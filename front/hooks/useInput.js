78bb5e815c437df77d6cd0ad305fd9fa585fb50e
hiphoplsy <hiphoplsy@naver.com>
2021-09-23 15:31:35 +0900

    ADD: custom hook

---
+++ front/hooks/useInput.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+export default (initValue = null) => {
+  const [value, setValue] = useState(initValue);
+  const handler = useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
+  return [value, handler, setValue];
+};
