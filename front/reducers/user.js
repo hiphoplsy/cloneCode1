cce0b24910f214fd7e45f50058bd0f9e239ba843
hiphoplsy <hiphoplsy@naver.com>
2021-09-24 14:48:43 +0900

    UPDATE: userReducer

--- front/reducers/user.js
+++ front/reducers/user.js
@@ -9,6 +9,9 @@ export const initialState = {
     loginLoading: false,
     loginDone: false,
     loginError: null,
+    signUpLoading: false,
+    singUpDone: false,
+    singUpError: null,
   }
 };
 
@@ -24,8 +27,26 @@ export const LOGIN_REQUEST = 'LOGIN_REQUEST';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAILURE = 'LOGIN_FAILURE';
 
+export const SIGNUP_REQUEST = 'SIGNUP_REQUEST';
+export const SIGNUP_SUCCESS = 'SIGNUP_SUCCESS';
+export const SIGNUP_FAILURE = 'SIGNUP_FAILURE';
+
 const reducer = (state=initialState, action) => produce(state, (draft) => {
   switch(action.type) {
+    case SIGNUP_REQUEST:
+      draft.signUpLoading = true;
+      draft.signUpDone = false;
+      draft.singUpError = null;
+      break;
+    case SIGNUP_SUCCESS:
+      draft.signUpLoading = false;
+      draft.signUpDone = true;
+      draft.me = dummyUser(action.data);
+      break;
+    case SIGNUP_FAILURE:
+      draft.signUpLoading = false;
+      draft.singUpError = action.error;
+      break;
     case LOGIN_REQUEST:
       draft.loginLoading = true;
       draft.loginDone = false;
@@ -34,7 +55,7 @@ const reducer = (state=initialState, action) => produce(state, (draft) => {
     case LOGIN_SUCCESS:
       draft.loginLoading = false;
       draft.loginDone = true;
-      draft.me = action.data;
+      draft.me = dummyUser(action.data);
       break;
     case LOGIN_FAILURE:
       draft.loginDone = false;
