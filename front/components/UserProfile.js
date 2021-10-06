e2f4c3cc4e8283694495f4fe62929a4d311fbe5d
hiphoplsy <hiphoplsy@naver.com>
2021-10-06 15:47:02 +0900

    ADD: UserProfile

---
+++ front/components/UserProfile.js
@@ -0,0 +1,46 @@
+import React, { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Avatar, Card, Button } from 'antd';
+
+import { LOGOUT_REQUEST } from '../reducers/user';
+
+const UserProfile = () => {
+  const dispatch = useDispatch()
+  const { me, logoutLoading } = useSelector((state) => state.user);
+
+  const onLogout = useCallback(() => {
+    dispatch({
+      type: LOGOUT_REQUEST,
+    })
+  }, []); 
+
+  return (
+    <Card
+      actions={[
+        <div key="twit">
+          짹짹
+          <br />
+          {me.Posts.length}
+        </div>,
+        <div key="followings">
+          팔로잉
+          <br />
+          {me.Followings.length}
+        </div>,
+        <div key="followers">
+          팔로워
+          <br />
+          {me.Followers.length}
+        </div>,
+      ]}
+    >
+      <Card.Meta 
+        avatar={<Avatar>{me.nickname[0]}</Avatar>}
+        title={me.nickname}
+      />
+      <Button loading={logoutLoading} onClick={onLogout}>로그아웃</Button>
+    </Card>
+  )
+};
+
+export default UserProfile;
