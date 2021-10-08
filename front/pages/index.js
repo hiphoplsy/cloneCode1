e5b2e018f64122b7983a8433bdeb8af671b2a58b
hiphoplsy <hiphoplsy@naver.com>
2021-10-08 13:27:17 +0900

    ADD: LOAD_POSTS

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import AppLayout from '../components/AppLayout';
 import PostForm  from '../components/PostForm';
 
+import { LOAD_POSTS_REQUEST } from '../reducers/post';
+
 
 const Home = () => {
+  const dispatch = useDispatch();
   const { isLoggedIn } = useSelector((state) => state.user);
   const { mainPosts } = useSelector((state) => state.post);
+
+  useEffect(() => {
+    dispatch({
+      type: LOAD_POSTS_REQUEST,
+    })
+  }, []);
 
   return (
     <AppLayout>
